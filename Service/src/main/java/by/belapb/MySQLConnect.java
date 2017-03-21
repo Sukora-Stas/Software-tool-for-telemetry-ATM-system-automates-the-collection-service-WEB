c36e55aaccbe43adf3d27d17e0f228f087a5dc28
Sukora-Stas <stasiksukora@mail.ru>
2017-03-21 10:32:22 +0300

    model 2 hibernate

---
+++ Service/src/main/java/by/belapb/MySQLConnect.java
@@ -0,0 +1,38 @@
+package by.belapb;
+
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class MySQLConnect {
+    public static void main(String[] args) {
+
+        System.out.println("------- Проверка подключения к MySQL -------");
+
+        Connection connection = null;
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:2016/it_academy",
+                    "root","");
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQLConnect.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+
+        if(null != connection) {
+            System.out.println("------- Подключение установлено -------");
+        } else {
+            System.out.println("------- Подключение НЕ установлено -------");
+        }
+    }
+}
--- Web/src/main/webapp/index.jsp
+++ Web/src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: stasi
+  Date: 17.03.2017
+  Time: 16:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
 <h2>Hello World! TEST</h2>
+<p>Здарова</p>
 </body>
 </html>
