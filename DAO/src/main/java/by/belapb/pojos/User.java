95a05d44226e02b052959bfc7975e4fe3560367d
Sukora-Stas <stasiksukora@mail.ru>
2017-04-11 16:55:40 +0300

    project

--- DAO/src/main/java/by/belapb/pojos/User.java
+++ DAO/src/main/java/by/belapb/pojos/User.java
@@ -1,8 +1,8 @@
 package by.belapb.pojos;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.security.PrivateKey;
 
 /**
  * Created by Sukora Stas.
@@ -13,9 +13,27 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = 3651232138379038813L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
+    @Column(name = "id_Role")
+    private Integer id_role;
 
+    @Column(name = "Логин")
+    private String Login;
 
+    @Column(name = "Пароль")
+    private String Password;
+
+    @Column(name = "Фамилия")
+    private String SurName;
+
+    @Column(name = "Имя")
+    private String FirstName;
+
+    @Column(name = "Отчество")
+    private String MiddleName
 
 
 }
