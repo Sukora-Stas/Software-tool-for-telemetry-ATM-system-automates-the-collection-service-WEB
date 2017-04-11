693055b4300ae4951704d9e1a3c48a4a34675c31
Sukora-Stas <stasiksukora@mail.ru>
2017-04-11 20:12:42 +0300

    project add dao

---
+++ DAO/src/main/java/by/belapb/dao/RoleDao.java
@@ -0,0 +1,13 @@
+package by.belapb.dao;
+
+import by.belapb.pojos.Role;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class RoleDao extends BaseDao<Role> {
+    private static Logger log = Logger.getLogger(RoleDao.class);
+
+
+}
---
+++ DAO/src/main/java/by/belapb/dao/UserDao.java
@@ -0,0 +1,11 @@
+package by.belapb.dao;
+
+import by.belapb.pojos.User;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Sukora Stas.
+ */
+public class UserDao extends BaseDao<User> {
+    private static Logger log = Logger.getLogger(UserDao.class);
+}
